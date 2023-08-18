f7cbcfec4d72455c3cdf99e34ba8072e927c2f0d
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-08-18 17:28:44 -0500

    Clase Auto

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,46 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		
+		int numAsientos = 0;
+		
+		for (int i = 0; i < asientos.length; i++)
+		{ 
+			if (asientos[i] != null){
+				
+				numAsientos ++;
+			}
+			}
+		return numAsientos;
+		}
+	
+	String verificarintegridad() 
+	{
+		if(registro == motor.registro){
+			
+			for (int i = 0; i < asientos.length; i++){
+				
+				if (asientos[i] != null){
+					
+					if (registro != asientos[i].registro);{
+						
+						return("Las piezas no son originales");
+					}
+				}
+			}
+		return ("Auto Original");
+		}
+		else {
+			return("Las piezas no son originales");
+		}
+	}
+}
